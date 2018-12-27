521ef5825b6fc4b884ffdb58e1cfb8d22a3219d4
Collin-St <collin.street.dr@gmail.com>
2018-12-27 10:08:03 -0800

    cleanup

--- spec/db.test.js
+++ spec/db.test.js
@@ -2,12 +2,3 @@ const mongoose = require('mongoose');
 const request = require('supertest');
 const Info = require('../db/index.js');
 
-// incomplete
-// describe('', () => {
-//   beforeAll(() => {
-//       mongoDB.connect();
-//   });
-//   afterAll((done) => {
-//       mongoDB.disconnect(done);
-//   });
-// })
