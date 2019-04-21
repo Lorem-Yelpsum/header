7aa09ea6210b4854575ce00538fab7603e9795c9
collin-st <collin.street.dr@gmail.com>
2019-04-21 14:00:24 -0700

    layout and functionality quickfix

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,68 +1,102 @@
-import React from 'react';
+import React from 'react'
 // import Portal from './Portal.jsx';
 // import ModalController from './ModalController.jsx';
-import AddPhoto from './AddPhoto/AddPhoto.jsx';
-import Share from './Share/Share.jsx';
-import Save from './Save/Save.jsx';
-import WriteReview from './WriteReview/WriteReview.jsx';
-import MapBox from './MapBox/MapBox.jsx';
-import BizInfo from './BizInfo/BizInfo.jsx';
-import CSSModules from 'react-css-modules';
-import style from './app.css';
+import AddPhoto from './AddPhoto/AddPhoto.jsx'
+import Share from './Share/Share.jsx'
+import Save from './Save/Save.jsx'
+import WriteReview from './WriteReview/WriteReview.jsx'
+import MapBox from './MapBox/MapBox.jsx'
+import BizInfo from './BizInfo/BizInfo.jsx'
+import CSSModules from 'react-css-modules'
+import style from './app.css'
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      biz: {},
-      showModal: false,
+      street: '',
+      city: '',
+      state: '',
+      zip: '',
+      phone: '',
+      category: '',
+      rest_name: '',
+      pricePoint: null,
+      totalReviews: null,
+      showModal: false
     }
-    this.handleShow = this.handleShow.bind(this);
-    this.handleHide = this.handleHide.bind(this);
+    this.handleShow = this.handleShow.bind(this)
+    this.handleHide = this.handleHide.bind(this)
   }
 
   componentDidMount() {
-    let getRoute = window.restId !== undefined ? `/http://127.0.0.1:3003/restaurantInfo/${window.restId}` : `http://127.0.0.1:3003/restaurantInfo/1`;
-    fetch(getRoute)
-    .then(response => {
-      return response.json();
-    }).then(data => {
-      this.setState({
-        biz: data[0]
-      });
-    });
+    let restId = window.location.href.split('/')[4]
+    let route =
+      restId < 101 && restId >= 0
+        ? `http://127.0.0.1:3003/restaurantInfo/${restId}`
+        : `http://127.0.0.1:3003/restaurantInfo/0`
+
+    fetch(route)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        console.log(data[0])
+        data = data[0]
+        this.setState({
+          street: data.address.street,
+          city: data.address.city,
+          state: data.address.city,
+          zip: data.address.zip,
+          phone: data.phone,
+          category: data.category,
+          rest_name: data.rest_name,
+          pricePoint: data.price_point,
+          totalReviews: data.totalReviews.total
+        })
+      })
   }
 
   handleShow() {
-    console.log(this.state.biz)
-    this.setState({showModal: true});
+    this.setState({ showModal: true })
   }
-  
+
   handleHide() {
-    this.setState({showModal: false});
-    console.log(this.state);
+    this.setState({ showModal: false })
+    console.log(this.state)
   }
 
   render() {
     return (
       <div styleName="container">
+        <BizInfo />
+        <div styleName="comps-container">
+          <div styleName="biz-header">
+            <h1 styleName="biz-title">{this.state.rest_name}</h1>
 
-          <BizInfo />
-          <div styleName="comps-container">
-            <div styleName="biz-header">
-              <h1 styleName="biz-title">Title</h1>
+            <Save />
+            <Share />
+            <AddPhoto />
+            <WriteReview onClick={this.handleShow} />
+          </div>
+          <div styleName="review-total">
+            <span>{this.state.totalReviews} reviews</span>
+          </div>
+          {/* <br /> */}
+          <div styleName="price-point">
+            <span>
+              $$ • <a href="#">{this.state.category}</a>
+            </span>
+          </div>
+          {/* <br /> */}
 
-          <Save />
-          <Share />
-          <AddPhoto />
-          <WriteReview onClick={this.handleShow}></WriteReview>
-            </div>
-            <div styleName="price-point">
-              <span>$$ •</span>
-            </div>
-            <br />
-          
-          <MapBox />
+          <MapBox
+            street={this.state.street}
+            city={this.state.city}
+            state={this.state.state}
+            zip={this.state.zip}
+            phone={this.state.phone}
+          />
         </div>
       </div>
     )
@@ -70,4 +104,4 @@ class App extends React.Component {
 }
 
 // export default App;
-export default CSSModules(App, style, {allowMultiple: true});
+export default CSSModules(App, style, { allowMultiple: true })
--- client/src/components/MapBox/MapBox.jsx
+++ client/src/components/MapBox/MapBox.jsx
@@ -1,32 +1,51 @@
-import React from 'react';
-import CSSModules from 'react-css-modules';
-import style from './mapbox.css';
-import Icon from '../icons/Icons.jsx';
+import React from 'react'
+import CSSModules from 'react-css-modules'
+import style from './mapbox.css'
+import Icon from '../icons/Icons.jsx'
 
 const MapBox = props => {
+  const { street, city, state, zip, phone } = props
   return (
     <div styleName="mapbox-container">
       <div styleName="mapbox-map">
-      <img src="https://s3.amazonaws.com/fec-sample-imgs/imgs/staticmap.png" styleName="mapimg" />
+        <img
+          src="https://s3.amazonaws.com/fec-sample-imgs/imgs/staticmap.png"
+          styleName="mapimg"
+        />
       </div>
       <div styleName="mapbox-text">
         <ul>
           <li>
             <Icon name={`18x18_marker`} height={18} width={18} fill="#666" />
-            <span styleName="mapbox-icons">address</span></li>
+            <span styleName="mapbox-icons">
+              {street}, {city}, {state} {zip.split('-')[0]}
+            </span>
+          </li>
           <li>
-            <Icon  name={`18x18_directions`} height={18} width={18} fill="#666" />
-            <a href="#"><span styleName="mapbox-icons">Get Directions</span></a></li>
+            <Icon
+              name={`18x18_directions`}
+              height={18}
+              width={18}
+              fill="#666"
+            />
+            <a href="#">
+              <span styleName="mapbox-icons">Get Directions</span>
+            </a>
+          </li>
           <li>
             <Icon name={`18x18_phone`} height={18} width={18} fill="#666" />
-            <span styleName="mapbox-icons">phone</span></li>
+            <span styleName="mapbox-icons">{phone.split('x')[0]}</span>
+          </li>
           <li>
             <Icon name={`18x18_mobile`} height={18} width={18} fill="#666" />
-            <a href="#"><span styleName="mapbox-icons">Send to your Phone</span></a></li>
+            <a href="#">
+              <span styleName="mapbox-icons">Send to your Phone</span>
+            </a>
+          </li>
         </ul>
       </div>
     </div>
   )
 }
 
-export default CSSModules(MapBox, style, {allowMultiple: true});
+export default CSSModules(MapBox, style, { allowMultiple: true })
